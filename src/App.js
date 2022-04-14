de69b7acaa154fcd369aff036141906452da0ae2
rel-ross <arielle@arielleross.com>
2022-04-14 21:09:08 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import spankpay, { PaymentCurrencyType } from "@spankchain-dev/spankpay-sdk";
 
-function App() {
+const App = () => {
+  const showSpankPay = async (fiatEnabled, guestOnly) => {
+    await spankpay.show({
+      username: "arielle",
+      message: "Money",
+      amount: "10",
+      currency: 'GBP',
+      // acceptedCurrencies: ['BTC', 'ETH'],
+      callbackUrl: 'https://eozbqullx2we6nn.m.pipedream.net',
+      description: 'Test Invoice Show',
+      metadata: { orderId: 'sc696969' },
+      fiatEnabled: fiatEnabled || false,
+      guestOnly: guestOnly || false,
+      allowPartial: false,
+      premiumPct: 0.04,
+      apiKey: 'pk_volatile_vision_ODCifGOg7rYefhn1HDA25xnAChxN4sJuv0FCWgoxr',
+      redirectUrl: 'http://localhost:3000/'
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      Hello, this is my app that integrates with Spankpay
+    <div>
+      <button onClick={() => showSpankPay(true, true)}>
+        Using the JS show method{" "}
+        <span role="img" aria-label="smile">
+          ❤️
+        </span>
+      </button>
+    </div>
     </div>
   );
-}
+};
 
 export default App;
